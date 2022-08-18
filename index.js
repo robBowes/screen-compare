133ab8a7e1c8d29bbfc7fdae1212fd633239caac
Robert Bowes <rbowes@zola.com>
2022-08-18 13:50:48 -0400

    Add screenshots folder

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const puppeteer = require("puppeteer");
 const ScreenshotTester = require("puppeteer-screenshot-tester");
 const yargs = require("yargs");
+const path = require("path");
+const fs = require("fs/promises");
 
 const test = async () => {
+  if (!fs.existsSync("screenshots")) {
+    fs.mkdirSync("screenshots");
+  }
+
   const tester = await ScreenshotTester(
     0.8,
     false,
@@ -17,21 +23,20 @@ const test = async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.setViewport({ width: 1920, height: 1080 });
-  await page.goto(`http://localhost/${yargs.argv.path}`, {
+
+  await page.goto(yargs.argv.path, {
     waitUntil: "networkidle0",
   });
-  //   await page.type('input[title="Search"]', 'Hello', { delay: 100 });
+  // await page.('input[title="Search"]', "Hello", { delay: 100 });
   // call our tester with browser page returned by puppeteer browser
   // second parameter is optional it's just a test name if provide that's filename
-  await tester(page, "test2", {
-    fullPage: true,
-  });
-  await page.goto(`http://localhost/${yargs.argv.path}`, {
-    waitUntil: "networkidle0",
-  });
-  const result = await tester(page, "test2", {
-    fullPage: true,
-  });
+  const result = await tester(
+    page,
+    path.join("screenshots", encodeURIComponent(yargs.argv.path)),
+    {
+      fullPage: true,
+    }
+  );
   await browser.close();
   console.log(result);
 };
